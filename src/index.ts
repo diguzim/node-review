fe68fd8d01e45b65bd97344b11a26806b9d475cd
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-05-18 15:55:47 -0300

    Initializing express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello world!!');
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+if (!process.env.PORT) {
+    console.log('Missing port environment variable');
+    process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
