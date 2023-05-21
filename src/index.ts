bd66f91e49585dd11fcfa4ab29e34aa2ecf8dfbe
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-05-21 11:40:08 -0300

    Configuring mongodb

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,22 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import mongoose from 'mongoose';
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_HOST as string, {
+    autoIndex: true,
+    dbName: process.env.MONGO_DATABASE as string
+}).then(
+    () => console.log('Connected to database')
+).catch(
+    () => {
+        console.log('Couldn\'t connect to database');
+        process.exit(1); 
+    }
+);
+    
 
 if (!process.env.PORT) {
     console.log('Missing port environment variable');
